790d861f98cb7acb1b575cffb03ce8fca7d9b2e2
Devesh Shetty <devesh.shetty007@gmail.com>
2017-05-15 02:22:01 +0530

    Added model object

---
+++ src/main/java/com/devesh/shetty/util/Constant.java
@@ -0,0 +1,11 @@
+package com.devesh.shetty.util;
+
+public class Constant {
+
+  public static final String SCHEMA_DETAIL_TABLE_NAME = "schema_detail";
+  
+  public static final String SCHEMA_ID_COLUMN_NAME = "schema_id";
+  
+  public static final String SCHEMA_PATH_COULMN_NAME = "schema_path";
+  
+}
