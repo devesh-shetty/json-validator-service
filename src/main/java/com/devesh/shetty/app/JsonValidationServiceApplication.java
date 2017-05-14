db4ad674458cd6daae4c82f5ff189f3ce1f6bc6c
Devesh Shetty <devesh.shetty007@gmail.com>
2017-05-15 01:03:08 +0530

    Initial commit

---
+++ src/main/java/com/devesh/shetty/app/JsonValidationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.devesh.shetty.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonValidationServiceApplication {
+
+	public static void main(String[] args) {
+	  SpringApplication.run(JsonValidationServiceApplication.class, args);
+	}
+}
