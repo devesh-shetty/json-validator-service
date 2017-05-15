22310efa9400217d046896f3b1d5d08ecd796af8
Devesh Shetty <devesh.shetty007@gmail.com>
2017-05-15 20:00:33 +0530

    Changes in ProcessingMessage

--- src/main/java/com/devesh/shetty/service/SchemaValidatorService.java
+++ src/main/java/com/devesh/shetty/service/SchemaValidatorService.java
@@ -19,6 +19,7 @@ import com.devesh.shetty.util.Constants;
 import com.devesh.shetty.util.ValidationUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingMessage;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
@@ -94,7 +95,12 @@ public class SchemaValidatorService implements ISchemaValidatorService {
     if (report.isSuccess()) {
       response = new ActionResponse(Constants.ACTION_VALIDATE, schemaId, Constants.STATUS_SUCCESS);
     } else {
-      response = new ActionResponse(Constants.ACTION_VALIDATE, schemaId, Constants.STATUS_ERROR, report.toString());
+
+      String msg = "";
+      for (final ProcessingMessage reportLine : report) {
+        msg += reportLine.toString() + "\n";
+      }
+      response = new ActionResponse(Constants.ACTION_VALIDATE, schemaId, Constants.STATUS_ERROR, msg);
     }
 
     return response;
